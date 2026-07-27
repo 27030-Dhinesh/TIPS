b66c7e3bde73f1e261d30b469545edee2ecf6041
Dhinesh <300329876+27030-Dhinesh@users.noreply.github.com>
2026-07-27 15:18:41 +0530

    feat: implement private methods to add and display products in view layer

--- src/InventoryManagementSystem/Program.cs
+++ src/InventoryManagementSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using InventoryManagementSystem.Repository;
+﻿using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Repository;
 using InventoryManagementSystem.Services;
 using static InventoryManagementSystem.ConsoleHelper;
 
@@ -58,6 +59,22 @@ namespace Assignments
 
         private static void HandleProductCreation(InventoryManager manager)
         {
+            string id = GetProductID("Enter the product ID:", "Invalid input for ID, try again.");
+            string name = GetProductName("Enter the name of the product:", "Invalid input for Name, try again.");
+            decimal price = GetPrice("Enter the price of the product:", "Invalid input for price, try again.");
+            int quantity = GetQuantity("Enter the quantity of the product:", "Invalid input for quantity, try again.");
+
+            Product product = new Product(id, name, price, quantity);
+
+            bool status = manager.AddProduct(product);
+            if (status)
+            {
+                Console.WriteLine("Product added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to add product; another product with same ID exists.");
+            }
         }
 
         private static void HandleProductEdit(InventoryManager manager)
@@ -70,6 +87,25 @@ namespace Assignments
 
         private static void HandleProductDisplay(InventoryManager manager)
         {
+            List<Product> products = manager.GetAllProducts();
+
+            if (products.Count == 0)
+            {
+                Console.WriteLine("Inventory is empty.");
+                Thread.Sleep(1000);
+                Console.Clear();
+                return;
+            }
+
+            Console.WriteLine("==============================");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+                Console.WriteLine("==============================");
+            }
+
+            Thread.Sleep(3000);
+            Console.Clear();
         }
 
         private static void HandleProductDeletion(InventoryManager manager)
