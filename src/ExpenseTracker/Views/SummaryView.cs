2983d278324e99b6c98ed3fbb16adf1643b98cb4
Dhinesh <300329876+27030-Dhinesh@users.noreply.github.com>
2026-08-11 18:48:32 +0530

    feat: add service and view for summary of entries

---
+++ src/ExpenseTracker/Views/SummaryView.cs
@@ -0,0 +1,29 @@
+﻿using ExpenseTracker.Services;
+using static ExpenseTracker.Helpers.ConsoleHelper;
+
+namespace ExpenseTracker.Views
+{
+    public class SummaryView
+    {
+        private readonly SummaryService _service;
+
+        public SummaryView(SummaryService service)
+        {
+            this._service = service;
+        }
+
+        public void GetSummary()
+        {
+            decimal totalIncome = this._service.GetTotalIncome();
+            decimal totalExpense = this._service.GetTotalExpense();
+            decimal netAmount = this._service.GetSummary();
+
+            WriteColorLine(
+                $@"Total Income: {totalIncome}
+Total Expense: {totalExpense}
+
+Net Balance: {netAmount}",
+                ConsoleColor.DarkYellow);
+        }
+    }
+}
