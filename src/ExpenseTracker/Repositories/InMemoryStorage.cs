4f3c1b260deb0d2eabf61c558b32c5c53fee37bf
Dhinesh <300329876+27030-Dhinesh@users.noreply.github.com>
2026-08-11 16:06:14 +0530

    docs: add docs for InMemoryStorage class

--- src/ExpenseTracker/Repositories/InMemoryStorage.cs
+++ src/ExpenseTracker/Repositories/InMemoryStorage.cs
@@ -3,27 +3,39 @@ using ExpenseTracker.Models;
 
 namespace ExpenseTracker.Repositories
 {
+    /// <summary>
+    /// Provides an in-memory implementation of <see cref="IRepository{T}"/> for storing
+    /// and managing entries during the application's lifetime.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of entry managed by the repository. The type must implement
+    /// <see cref="IEntry"/>.
+    /// </typeparam>
     internal class InMemoryStorage<T> : IRepository<T>
         where T : IEntry
     {
         private readonly List<T> _repository = new List<T>();
 
+        /// <inheritdoc/>
         public bool Add(T entry)
         {
             this._repository.Add(entry);
             return true;
         }
 
+        /// <inheritdoc/>
         public bool Contains(Guid id)
         {
             return this._repository.Any(entry => entry.Id == id);
         }
 
+        /// <inheritdoc/>
         public bool Delete(Guid id)
         {
             return this._repository.RemoveAll(entry => entry.Id == id) > 0;
         }
 
+        /// <inheritdoc/>
         public bool Edit(Guid id, T updatedEntry)
         {
             T oldEntry = this.GetById(id);
@@ -35,21 +47,25 @@ namespace ExpenseTracker.Repositories
             return true;
         }
 
+        /// <inheritdoc/>
         public List<T> GetAll()
         {
             return this._repository.Select(item => (T)item.Clone()).ToList();
         }
 
+        /// <inheritdoc/>
         public int GetEntriesCount()
         {
             return this._repository.Count;
         }
 
+        /// <inheritdoc/>
         public Guid GetIdByIndex(int index)
         {
             return this._repository[index].Id;
         }
 
+        /// <inheritdoc/>
         public bool IsEmpty()
         {
             return this._repository.Count == 0;
