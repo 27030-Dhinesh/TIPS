8b68bfc217ec41ad20b6b05b106a86d1ddb90564
Dhinesh <300329876+27030-Dhinesh@users.noreply.github.com>
2026-08-11 18:48:32 +0530

    feat: add service and view for summary of entries

---
+++ src/ExpenseTracker/Services/SummaryService.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseTracker.Interfaces;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Services
+{
+    public class SummaryService
+    {
+        private readonly IRepository<Income> _incomeRepository;
+        private readonly IRepository<Expense> _expenseRepository;
+
+        public SummaryService(
+            IRepository<Income> incomeRepository,
+            IRepository<Expense> expenseRepository)
+        {
+            this._incomeRepository = incomeRepository;
+            this._expenseRepository = expenseRepository;
+        }
+
+        public decimal GetTotalIncome()
+        {
+            return this._incomeRepository.GetTotalAmount();
+        }
+
+        public decimal GetTotalExpense()
+        {
+            return this._expenseRepository.GetTotalAmount();
+        }
+
+        public decimal GetSummary()
+        {
+            return this.GetTotalIncome() - this.GetTotalExpense();
+        }
+    }
+}
